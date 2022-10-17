cd95adac83cd4a515ea23b06e66f8f1b6f0df990
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-17 12:21:19 -0400

    import Share component to be displayed in feed

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,9 +1,14 @@
-import {} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import {} from "@material-ui/icons";
+import Share from "../share/Share";
 import "./Feed.scss";
 
 function Feed() {
-  return <div className="feed">feed</div>;
+  return (
+    <Container className="feed">
+      <Share />
+    </Container>
+  );
 }
 
 export default Feed;
