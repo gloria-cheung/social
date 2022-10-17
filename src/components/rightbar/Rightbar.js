18a2a8b63642b7da76bbfaf0b9b4ed65891958fc
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-17 16:23:02 -0400

    refactor to have online as its own component to clean up rightbar

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -1,26 +1,9 @@
 import { Container, Image, ListGroup } from "react-bootstrap";
 import { Users } from "../../dummyData";
+import Online from "../online/Online";
 import "./Rightbar.scss";
 
 function Rightbar() {
-  const users = Users.map((user) => (
-    <ListGroup.Item
-      as="li"
-      className="border-0 d-flex align-items-center"
-      key={user.id}
-    >
-      <div className="onlineFriendsImageContainer">
-        <Image
-          className="profilePic me-2"
-          src={user.profilePicture}
-          alt="profilepic"
-        />
-        <span className="online"></span>
-      </div>
-      {user.username}
-    </ListGroup.Item>
-  ));
-
   return (
     <Container className="rightbar pt-3">
       <Container className="birthdayContainer d-flex">
@@ -39,7 +22,7 @@ function Rightbar() {
       <Container className="friendListContainer">
         <h5>Online Friends</h5>
         <ListGroup as="ul" className="border-0">
-          {users}
+          <Online usersData={Users} />
         </ListGroup>
       </Container>
     </Container>
