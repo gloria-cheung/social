da95ceef720b35ac1f6337cc72ad2bf2282eeb32
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-17 16:21:40 -0400

    refactor to have online as its own component to clean up rightbar

---
+++ src/components/online/Online.js
@@ -0,0 +1,28 @@
+import { Image, ListGroup } from "react-bootstrap";
+import "./Online.scss";
+
+function Online(props) {
+  const { usersData } = props;
+
+  const users = usersData.map((user) => (
+    <ListGroup.Item
+      as="li"
+      className="border-0 d-flex align-items-center"
+      key={user.id}
+    >
+      <div className="onlineFriendsImageContainer">
+        <Image
+          className="profilePic me-2"
+          src={user.profilePicture}
+          alt="profilepic"
+        />
+        <span className="online"></span>
+      </div>
+      {user.username}
+    </ListGroup.Item>
+  ));
+
+  return <>{users}</>;
+}
+
+export default Online;
