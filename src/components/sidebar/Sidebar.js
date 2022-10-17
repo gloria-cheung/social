152e189759c647b9033202eedb38d29b8e456c06
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-17 10:27:07 -0400

    change markup to ul and li instead of using divs

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -16,42 +16,42 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <Container className="pt-3 pb-3">
-        <ListGroup variant="flush">
-          <ListGroup.Item>
+        <ListGroup as="ul" variant="flush">
+          <ListGroup.Item as="li">
             <RssFeedOutlined className="listIcon" />
             Feed
           </ListGroup.Item>
-          <ListGroup.Item>
+          <ListGroup.Item as="li">
             <ChatOutlined className="listIcon" />
             Chats
           </ListGroup.Item>
-          <ListGroup.Item>
+          <ListGroup.Item as="li">
             <PlayCircleOutlineOutlined className="listIcon" /> Videos
           </ListGroup.Item>
-          <ListGroup.Item>
+          <ListGroup.Item as="li">
             <GroupOutlined className="listIcon" />
             Groups
           </ListGroup.Item>
-          <ListGroup.Item>
+          <ListGroup.Item as="li">
             <BookmarkBorderOutlined className="listIcon" /> Bookmarks
           </ListGroup.Item>
-          <ListGroup.Item>
+          <ListGroup.Item as="li">
             <HelpOutlineOutlined className="listIcon" /> Questions
           </ListGroup.Item>
-          <ListGroup.Item>
+          <ListGroup.Item as="li">
             <WorkOutlineOutlined className="listIcon" /> Jobs
           </ListGroup.Item>
-          <ListGroup.Item>
+          <ListGroup.Item as="li">
             <EventOutlined className="listIcon" /> Events
           </ListGroup.Item>
-          <ListGroup.Item>
+          <ListGroup.Item as="li">
             <SchoolOutlined className="listIcon" /> Courses
           </ListGroup.Item>
         </ListGroup>
         <Button className="ps-5 pe-5">Show More</Button>
         <hr />
-        <ListGroup variant="flush">
-          <ListGroup.Item>
+        <ListGroup as="ul" variant="flush">
+          <ListGroup.Item as="li">
             <Image
               src="/assets/person/2.jpeg"
               alt="followingsImg"
@@ -59,7 +59,7 @@ function Sidebar() {
             />
             Jane Doe
           </ListGroup.Item>
-          <ListGroup.Item>
+          <ListGroup.Item as="li">
             <Image
               src="/assets/person/2.jpeg"
               alt="followingsImg"
@@ -67,7 +67,7 @@ function Sidebar() {
             />
             Jane Doe
           </ListGroup.Item>
-          <ListGroup.Item>
+          <ListGroup.Item as="li">
             <Image
               src="/assets/person/2.jpeg"
               alt="followingsImg"
