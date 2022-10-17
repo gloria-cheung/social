8f23ee453da20c88e8e7f15749293fb11e8c49fa
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-17 15:42:06 -0400

    import users and posts and map over each post to find correct user and display post contents

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -5,95 +5,43 @@ import {
   ChatBubbleOutlineOutlined,
   SendOutlined,
 } from "@material-ui/icons";
+import { Users, Posts } from "../../dummyData";
 import "./Post.scss";
 
 function Post() {
+  const posts = Posts.map((post) => (
+    <Card className="border-0 mt-3">
+      <Card.Header className="d-flex align-items-center justify-content-between postHeader border-0">
+        <div className="postHeaderLeft">
+          <Image
+            className="profilePic me-2"
+            src={Users.find((user) => user.id === post.userId).profilePicture}
+            alt="profilepic"
+          />
+          {Users.find((user) => user.id === post.userId).username}{" "}
+          <span className="muted">{post.date}</span>
+        </div>
+        <div className="postHeaderRight">
+          <MoreVert />
+        </div>
+      </Card.Header>
+
+      <Card.Img variant="top" src={post.photo} />
+
+      <Card.Body>
+        <div className="cardImgFooter">
+          <FavoriteBorderOutlined className="cardImgFooterIcon" />
+          <ChatBubbleOutlineOutlined className="cardImgFooterIcon" />
+          <SendOutlined className="cardImgFooterIcon" />
+          <p>Liked by {post.like} people</p>
+        </div>
+        <Card.Text>{post.desc && post.desc}</Card.Text>
+      </Card.Body>
+    </Card>
+  ));
+
   return (
-    <Container className="postsContainer ps-0 pe-0 pt-5">
-      <Card className="border-0 mt-3">
-        <Card.Header className="d-flex align-items-center justify-content-between postHeader border-0">
-          <div className="postHeaderLeft">
-            <Image
-              className="profilePic me-2"
-              src="/assets/person/1.jpeg"
-              alt="profilepic"
-            />
-            Gloria Cheung <span className="muted">5 mins ago</span>
-          </div>
-          <div className="postHeaderRight">
-            <MoreVert />
-          </div>
-        </Card.Header>
-
-        <Card.Img variant="top" src="/assets/post/1.jpeg" />
-
-        <Card.Body>
-          <div className="cardImgFooter">
-            <FavoriteBorderOutlined className="cardImgFooterIcon" />
-            <ChatBubbleOutlineOutlined className="cardImgFooterIcon" />
-            <SendOutlined className="cardImgFooterIcon" />
-            <p>Liked by 32 people</p>
-          </div>
-          <Card.Text>Hi! this is my first post.</Card.Text>
-        </Card.Body>
-      </Card>
-
-      <Card className="border-0 mt-3">
-        <Card.Header className="d-flex align-items-center justify-content-between postHeader border-0">
-          <div className="postHeaderLeft">
-            <Image
-              className="profilePic me-2"
-              src="/assets/person/1.jpeg"
-              alt="profilepic"
-            />
-            Gloria Cheung <span className="muted">5 mins ago</span>
-          </div>
-          <div className="postHeaderRight">
-            <MoreVert />
-          </div>
-        </Card.Header>
-
-        <Card.Img variant="top" src="/assets/post/1.jpeg" />
-
-        <Card.Body>
-          <div className="cardImgFooter">
-            <FavoriteBorderOutlined className="cardImgFooterIcon" />
-            <ChatBubbleOutlineOutlined className="cardImgFooterIcon" />
-            <SendOutlined className="cardImgFooterIcon" />
-            <p>Liked by 32 people</p>
-          </div>
-          <Card.Text>Hi! this is my first post.</Card.Text>
-        </Card.Body>
-      </Card>
-
-      <Card className="border-0 mt-3">
-        <Card.Header className="d-flex align-items-center justify-content-between postHeader border-0">
-          <div className="postHeaderLeft">
-            <Image
-              className="profilePic me-2"
-              src="/assets/person/1.jpeg"
-              alt="profilepic"
-            />
-            Gloria Cheung <span className="muted">5 mins ago</span>
-          </div>
-          <div className="postHeaderRight">
-            <MoreVert />
-          </div>
-        </Card.Header>
-
-        <Card.Img variant="top" src="/assets/post/1.jpeg" />
-
-        <Card.Body>
-          <div className="cardImgFooter">
-            <FavoriteBorderOutlined className="cardImgFooterIcon" />
-            <ChatBubbleOutlineOutlined className="cardImgFooterIcon" />
-            <SendOutlined className="cardImgFooterIcon" />
-            <p>Liked by 32 people</p>
-          </div>
-          <Card.Text>Hi! this is my first post.</Card.Text>
-        </Card.Body>
-      </Card>
-    </Container>
+    <Container className="postsContainer ps-0 pe-0 pt-5">{posts}</Container>
   );
 }
 
