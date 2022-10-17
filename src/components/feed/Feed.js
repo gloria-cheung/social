ff030c2082c11a98ce767b33b92d6ee96b1e112f
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-17 13:52:29 -0400

    import Post component

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,12 +1,14 @@
 import { Container } from "react-bootstrap";
 import {} from "@material-ui/icons";
 import Share from "../share/Share";
+import Post from "../post/Post";
 import "./Feed.scss";
 
 function Feed() {
   return (
     <Container className="feed">
       <Share />
+      <Post />
     </Container>
   );
 }
