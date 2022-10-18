a2875dd6a9238f7d4fefadbe6ef08a34053b1e0a
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-18 11:45:54 -0400

    create profile component using topbar, sidebar, feed, rightbar, and coverphoto with profile in righttop

---
+++ src/pages/profile/Profile.js
@@ -0,0 +1,41 @@
+import { Card } from "react-bootstrap";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./Profile.scss";
+
+function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="right">
+          <div className="rightTop">
+            <img
+              src="assets/post/3.jpeg"
+              alt="coverPicture"
+              className="coverPicture"
+            />
+            <div className="coverPictureBottom">
+              Sarak Kocaoglu
+              <p>Hello my friends!</p>
+            </div>
+            <img
+              src="assets/person/3.jpeg"
+              alt="profilePic"
+              className="profilePic"
+            />
+          </div>
+          <div className="rightBottom">
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Profile;
