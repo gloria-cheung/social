8af318233be9104e20f8a2b4e78c3f32d4710ab7
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-18 14:03:57 -0400

    use react router dom and import router, switch and route to display diff routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
@@ -6,7 +7,24 @@ import Register from "./pages/register/Register";
 import "./App.scss";
 
 function App() {
-  return <Register />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
