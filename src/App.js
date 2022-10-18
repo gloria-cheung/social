3369f570a9d9356e0594b245fa57d8ee7f9f9e10
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-18 11:43:25 -0400

    import Profile page to render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 import "./App.scss";
 
 function App() {
-  return <Home />;
+  return <Profile />;
 }
 
 export default App;
