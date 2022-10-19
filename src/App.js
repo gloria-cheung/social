a20fc2e60a7b01ae05ef542e26a127b53b837e74
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-19 14:05:09 -0400

    import useContext to consume AuthContext and render components based on whether currentUser is defined

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
@@ -7,17 +14,19 @@ import Register from "./pages/register/Register";
 import "./App.scss";
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home />
+          {currentUser ? <Home /> : <Register />}
         </Route>
         <Route path="/login">
-          <Login />
+          {currentUser ? <Redirect to="/" /> : <Login />}
         </Route>
         <Route path="/register">
-          <Register />
+          {currentUser ? <Redirect to="/" /> : <Register />}
         </Route>
         <Route path="/profile/:username">
           <Profile />
