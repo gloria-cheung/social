6f78f10bd4a8eb027a619e2005ccfa072c0b9b3f
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-19 12:22:46 -0400

    define action and return payload

---
+++ src/context/AuthActions.js
@@ -0,0 +1,11 @@
+export const LoginStart = (userCredentials) => {
+  return { type: "LOGIN_START" };
+};
+
+export const LoginSuccess = (user) => {
+  return { type: "LOGIN_SUCCESS", payload: user };
+};
+
+export const LoginFailure = (error) => {
+  return { type: "LOGIN_FAILURE", payload: error };
+};
