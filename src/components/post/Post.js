9b81d08b9205a7c99fd2d5352a6f6c0457289731
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-22 09:40:51 -0400

    after deleting posts, call resetposts to refetch the new posts

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -17,10 +17,9 @@ import { fetchUserbyId, likePost, deletePost } from "../../apiCalls";
 import { format } from "timeago.js";
 
 function Post(props) {
-  const { post } = props;
+  const { post, resetPosts } = props;
   const [likes, setLikes] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState();
-  const [isDeleted, setIsDeleted] = useState();
   const [user, setUser] = useState({});
 
   const { currentUser } = useContext(AuthContext);
@@ -59,95 +58,87 @@ function Post(props) {
     }
   };
 
-  const deleteHandler = () => {
-    deletePost(post._id, currentUser._id);
-    setIsDeleted(true);
+  const deleteHandler = async () => {
+    await deletePost(post._id, currentUser._id);
+    resetPosts();
   };
 
   return (
     <Container className="p-0">
-      {!isDeleted && (
-        <>
-          <Card className="border-0 mt-3">
-            <Card.Header className="d-flex align-items-center justify-content-between postHeader border-0">
-              <div className="postHeaderLeft">
-                <Link to={`/profile/${user.username}`}>
-                  <Image
-                    className="profilePic me-2"
-                    src={user.profilePicture || PF + "/noAvatar.png"}
-                    alt="profilepic"
-                  />
-                </Link>
-                {user.username}
-                <span className="muted">{format(post.createdAt)}</span>
-              </div>
-              <div className="postHeaderRight">
-                <MoreVert onClick={handleShow} className="options" />
-              </div>
-            </Card.Header>
+      <Card className="border-0 mt-3">
+        <Card.Header className="d-flex align-items-center justify-content-between postHeader border-0">
+          <div className="postHeaderLeft">
+            <Link to={`/profile/${user.username}`}>
+              <Image
+                className="profilePic me-2"
+                src={user.profilePicture || PF + "/noAvatar.png"}
+                alt="profilepic"
+              />
+            </Link>
+            {user.username}
+            <span className="muted">{format(post.createdAt)}</span>
+          </div>
+          <div className="postHeaderRight">
+            <MoreVert onClick={handleShow} className="options" />
+          </div>
+        </Card.Header>
 
-            <Card.Img variant="top" src={post.img} />
+        <Card.Img variant="top" src={post.img} />
 
-            <Card.Body>
-              <div className="cardImgFooter">
-                {isLiked ? (
-                  <FavoriteOutlined
-                    className="cardImgFooterIcon"
-                    onClick={likeHandler}
-                    htmlColor="salmon"
-                  />
-                ) : (
-                  <FavoriteBorderOutlined
-                    className="cardImgFooterIcon"
-                    onClick={likeHandler}
-                  />
-                )}
+        <Card.Body>
+          <div className="cardImgFooter">
+            {isLiked ? (
+              <FavoriteOutlined
+                className="cardImgFooterIcon"
+                onClick={likeHandler}
+                htmlColor="salmon"
+              />
+            ) : (
+              <FavoriteBorderOutlined
+                className="cardImgFooterIcon"
+                onClick={likeHandler}
+              />
+            )}
 
-                <ChatBubbleOutlineOutlined className="cardImgFooterIcon" />
-                <SendOutlined className="cardImgFooterIcon" />
-                <p>
-                  Liked by {likes === 1 ? `${likes} person` : `${likes} people`}
-                </p>
-              </div>
-              <Card.Text>{post.desc && post.desc}</Card.Text>
-            </Card.Body>
-          </Card>
-          <Modal show={show} onHide={handleClose} size="sm">
-            <Modal.Body>
-              <ListGroup variant="flush">
-                <ListGroup.Item
-                  onClick={likeHandler}
-                  className="modalItem"
-                  action
-                >
-                  <FavoriteBorderOutlined />
-                  Like post
-                </ListGroup.Item>
-                <ListGroup.Item className="modalItem" action>
-                  <BookmarkBorder />
-                  Save post
-                </ListGroup.Item>
-                {user.username !== currentUser.username && (
-                  <ListGroup.Item className="modalItem" action>
-                    <ReportProblemOutlined />
-                    Report post
-                  </ListGroup.Item>
-                )}
-                {user.username === currentUser.username && (
-                  <ListGroup.Item
-                    onClick={deleteHandler}
-                    className="modalItem"
-                    action
-                  >
-                    <DeleteOutlined />
-                    Delete post
-                  </ListGroup.Item>
-                )}
-              </ListGroup>
-            </Modal.Body>
-          </Modal>
-        </>
-      )}
+            <ChatBubbleOutlineOutlined className="cardImgFooterIcon" />
+            <SendOutlined className="cardImgFooterIcon" />
+            <p>
+              Liked by {likes === 1 ? `${likes} person` : `${likes} people`}
+            </p>
+          </div>
+          <Card.Text>{post.desc && post.desc}</Card.Text>
+        </Card.Body>
+      </Card>
+      <Modal show={show} onHide={handleClose} size="sm">
+        <Modal.Body>
+          <ListGroup variant="flush">
+            <ListGroup.Item onClick={likeHandler} className="modalItem" action>
+              <FavoriteBorderOutlined />
+              Like post
+            </ListGroup.Item>
+            <ListGroup.Item className="modalItem" action>
+              <BookmarkBorder />
+              Save post
+            </ListGroup.Item>
+            {user.username !== currentUser.username && (
+              <ListGroup.Item className="modalItem" action>
+                <ReportProblemOutlined />
+                Report post
+              </ListGroup.Item>
+            )}
+            {user.username === currentUser.username && (
+              <ListGroup.Item
+                onClick={deleteHandler}
+                className="modalItem"
+                action
+              >
+                <DeleteOutlined />
+                Delete post
+              </ListGroup.Item>
+            )}
+          </ListGroup>
+        </Modal.Body>
+      </Modal>
     </Container>
   );
 }
