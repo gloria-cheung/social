f4ff3ea8eb53499628ba0886049c9b19decaca15
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-22 09:40:03 -0400

    add func to reset posts and pass to share and post component to call to rerender posts after update

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -29,7 +29,12 @@ function Feed(props) {
         const res = username
           ? await getPostsForProfile(profileUserId)
           : await getPostsforTimeline(currentUser._id);
-        setPosts(res);
+
+        // sort posts by newest first
+        const sortedPosts = res.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        });
+        setPosts(sortedPosts);
       } catch (err) {
         console.log(err);
       }
@@ -38,14 +43,30 @@ function Feed(props) {
     fetchPosts();
   }, [username, currentUser]);
 
+  const resetPosts = async () => {
+    try {
+      const res = username
+        ? await getPostsForProfile(currentUser._id)
+        : await getPostsforTimeline(currentUser._id);
+
+      // sort posts by newest first
+      const sortedPosts = res.sort((p1, p2) => {
+        return new Date(p2.createdAt) - new Date(p1.createdAt);
+      });
+      setPosts(sortedPosts);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Container className="feed">
       {(!username || username === currentUser.username) && (
-        <Share currentUser={currentUser} />
+        <Share currentUser={currentUser} resetPosts={resetPosts} />
       )}
       <Container className="postsContainer ps-0 pe-0">
         {posts.map((post) => (
-          <Post post={post} key={post._id} />
+          <Post post={post} key={post._id} resetPosts={resetPosts}/>
         ))}
       </Container>
     </Container>
